47e37a9ca6cb54f2ee27ab28203e4ce510121429
akechsalim <akechsalim@gmail.com>
2024-11-14 17:24:16 +0300

    separated different features of the system into a humberger menu

--- community-service-management-system/src/main/java/com/akechsalim/community_service_management_system/repository/EventRepository.java
+++ community-service-management-system/src/main/java/com/akechsalim/community_service_management_system/repository/EventRepository.java
@@ -2,6 +2,8 @@ package com.akechsalim.community_service_management_system.repository;
 
 import com.akechsalim.community_service_management_system.model.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
 }
